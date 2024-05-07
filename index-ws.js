4e807b5964e2c67327fa3259ec882c9a31fd8ca4
calin <calin>
2024-05-07 23:26:46 +0300

    create websocket

--- index-ws.js
+++ index-ws.js
@@ -6,7 +6,37 @@ app.get("/", function (req, res) {
   res.sendFile("index.html", { root: __dirname });
 });
 
-server.on("reqest", app);
+app.get("/test", function (req, res) {
+  res.sendFile("test.html", { root: __dirname });
+});
+
+server.on("request", app);
 server.listen(3000, () => {
   console.log("server started");
 });
+
+/** Begin Websockets */
+const webSocketServer = require("ws").Server;
+const wss = new webSocketServer({ server });
+
+wss.on("connection", function connection(ws) {
+  const numCLients = wss.clients.size;
+  console.log("clients connected:", numCLients);
+  console.log("client connected");
+  wss.broadcast(`Current visitors count: ${numCLients}`);
+  if (ws.readyState === ws.OPEN) {
+    ws.send("welcome");
+  }
+
+  ws.on("close", function close() {
+    wss.broadcast(`Current visitors count: ${numCLients}`);
+    console.log("client disconnected");
+    console.log("clients connected:", numCLients);
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach((client) => {
+    client.send(data);
+  });
+};
